11895f010dd27172f6babe6b42a641aa9efd0a51
Jayhonda <33628600+Jayhonda@users.noreply.github.com>
2020-09-20 21:23:27 -0400

    Added SignLocationManager functionality

--- src/com/csg/utils/commands/CommandSignLocations.java
+++ src/com/csg/utils/commands/CommandSignLocations.java
@@ -1,12 +1,38 @@
 package com.csg.utils.commands;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Scanner;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
+
+import com.csg.showdown.Showdown;
+import com.csg.utils.position.SignLocationManager;
 
 public class CommandSignLocations implements CommandExecutor {
+	
+	private static ArrayList<HashMap<String, ArrayList<Location>>> signGroupList = new ArrayList<HashMap<String, ArrayList<Location>>>();
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -15,21 +41,20 @@ public class CommandSignLocations implements CommandExecutor {
 			if(player.hasPermission("csa.signlocations")) {
 				// 
 				if(args.length >= 1) {
+					//File file = new File("\\plugins\\SignLocations\\");
 					// TODO: DO IDEAS IN COMMENTS
 					// IMPLEMENT THEM IN SignLocationManager
-					if(args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("create")) {
+					if(args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("create")
+							|| args[0].equalsIgnoreCase("edit") || args[0].equalsIgnoreCase("modify")) {
+						Bukkit.getServer().getPluginManager().registerEvents(new SignLocationManager(player, args[1]), Showdown.getPlugin());
 						// Instantiate a SignGroup
 						// Add it to a list of SignGroup
 						// SignGroup class - stores sign locations, exports to config
 						// Begin editing the SignGroup (place Player and SignGroup in static HashMap??)
 						// Should multiple people be able to edit?
 					}
-					else if(args[0].equalsIgnoreCase("edit") || args[0].equalsIgnoreCase("modify")) {
-						// Check if SignGroup exists (by name)
-						// Begin editing SignGroup
-						// Should multiple people be able to edit?
-					}
 					else if(args[0].equalsIgnoreCase("confirm") || args[0].equalsIgnoreCase("finish")) {
+						player.sendMessage("Listener list: " + Arrays.toString(HandlerList.getRegisteredListeners(Showdown.getPlugin()).toArray()));
 						// Check if player is editing
 						// Finalize everything, make sure it exports to config
 						// Delete dem signs
@@ -41,6 +66,101 @@ public class CommandSignLocations implements CommandExecutor {
 						// Remove from config
 						// ???
 						// Profit
+					}
+					else if(args[0].equalsIgnoreCase("test")) {
+						
+						Bukkit.getServer().getPluginManager().registerEvents(new SignLocationManager(player, args[1]), Showdown.getPlugin());
+						
+						/*
+						//HashMap<String, ArrayList<Location>> test = new HashMap<String,  ArrayList<Location>>();
+						ArrayList<Location> test_Locations = new ArrayList<Location>();
+						
+						test_Locations.add(player.getLocation());
+						test_Locations.add(player.getLocation().add(1, 0, 0));
+						test_Locations.add(player.getLocation().add(-1, 0, 0));
+						test_Locations.add(player.getLocation().add(0, 1, 0));
+						test_Locations.add(player.getLocation().add(0, -1, 0));
+						test_Locations.add(player.getLocation().add(0, 0, 1));
+						test_Locations.add(player.getLocation().add(0, 0, -1));
+						
+						//test.put(args[1], test_Locations);
+						
+						
+						File dir = new File("plugins/SignLocations/");
+						
+						if(dir.mkdir())
+							Bukkit.broadcastMessage(dir.getName() + " was generated in " + dir.getPath());
+						else
+							Bukkit.broadcastMessage(dir.getName() + " already exists in " + dir.getAbsolutePath());
+						
+						File file = new File(dir,"locations");
+						
+						try 
+						{
+							if(file.createNewFile())
+								Bukkit.broadcastMessage(file.getName() + " was generated in " + file.getPath());
+							else
+								Bukkit.broadcastMessage(file.getName() + " already exists in " + file.getPath());
+							
+							FileWriter fileWriter = new FileWriter(file);
+							
+							String toWrite = "[" + args[1] + "{";
+							
+							for(Location l : test_Locations)
+							{
+								toWrite += "(" + l.getBlockX() + "," + l.getBlockY() + "," + l.getBlockZ() + ", spawnZombie);";
+							}
+							
+							toWrite += "}]";
+							fileWriter.write(toWrite);
+							fileWriter.close();
+							
+							
+							Scanner fileRead = new Scanner(file);
+							String line = fileRead.nextLine();
+							
+							boolean keepGoing = true;
+							
+							while(keepGoing)
+							{
+								String focus = line.substring(line.indexOf("("), line.indexOf(";"));
+								
+								int x = Integer.parseInt(focus.substring(focus.indexOf("(") + 1, focus.indexOf(",")));
+								focus = focus.substring(focus.indexOf(",") + 1);
+								
+								int y = Integer.parseInt(focus.substring(0, focus.indexOf(",")));
+								focus = focus.substring(focus.indexOf(",") + 1);
+								
+								int z = Integer.parseInt(focus.substring(0, focus.indexOf(",")));
+								focus = focus.substring(focus.indexOf(",") + 1);
+								
+								String cmd = focus.substring(0, focus.indexOf(")"));
+								
+								Location loc = new Location(player.getWorld(), x, y, z);
+								
+								Block b = player.getWorld().getBlockAt(loc);
+								b.setType(Material.OAK_SIGN);
+								
+								Sign sign = (Sign) b.getState();
+								
+								sign.setLine(0, "[Location]");
+								sign.setLine(1, cmd);
+								sign.update();
+								
+								line = line.substring(line.indexOf(";") + 1, line.length());
+								Bukkit.broadcastMessage("Line: " + line);
+								
+								if(line.indexOf("}") == 0)
+									
+									keepGoing = false;
+							}
+						}
+						catch(IOException e)
+						{
+							e.printStackTrace();
+						}*/
+						
+						
 					}
 				}
 			}
--- src/com/csg/utils/position/SignLocationManager.java
+++ src/com/csg/utils/position/SignLocationManager.java
@@ -1,15 +1,195 @@
 package com.csg.utils.position;
 
-import org.bukkit.entity.Player;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class SignLocationManager {
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.SignChangeEvent;
+
+public class SignLocationManager implements Listener {
+	
+	public static ArrayList<Player> editorList = new ArrayList<Player>();
 
 	private Player editor;
 	private String name;
+	private int currentLine;
+	
+	private File dir = new File("plugins/SignLocations/");
+	private File file = new File(dir, "locations");
 	
 	public SignLocationManager(Player editor, String name) {
 		this.editor = editor;
 		this.name = name;
+		this.currentLine = checkIfExists();
+		
+		editorList.add(editor);
+	}
+	
+	@EventHandler
+	public void onPlaceSign(BlockPlaceEvent e) {
+		if(!e.getPlayer().equals(this.editor))
+			return;
+		
+		Player player = e.getPlayer();
+		
+		if(e.getBlock().getType() != Material.OAK_SIGN)
+			return;
+		
+		player.sendMessage("Get sign");
+		
+		Sign sign = (Sign) e.getBlock().getState();
+		
+		player.sendMessage("Got sign");
+		
+		sign.setLine(0, "[Location]");
+		
+		//if(!sign.getLine(0).equalsIgnoreCase("[Location]"))
+			//return;
+		
+		player.sendMessage("Placed sign location");
+		//updateFile(sign.getLocation(), sign.getLine(1));
+	}
+	
+	@EventHandler
+	public void onSignEdit(SignChangeEvent e) {
+		if(!e.getPlayer().equals(this.editor))
+			return;
+		
+		Sign sign = (Sign) e.getBlock().getState();
+		
+		if(!e.getLine(0).equalsIgnoreCase("[Location]"))
+		{
+			e.setLine(0, "[Location]");
+			e.getBlock().getState().update();
+		}
+		
+		updateFile(sign.getLocation(), e.getLines()[1]);
+	}
+	
+	public int appendFile(int line) {
+		editor.sendMessage("Appending new file " + line);
+		try {
+			//FileWriter fileWriter = new FileWriter(file);
+			/*
+			ArrayList<String> lines = new ArrayList<String>(Files.readAllLines(file.toPath()));
+			editor.sendMessage("lines: " + Arrays.toString(lines.toArray()) + " is empty? " + lines.isEmpty());
+			String toWrite = "";
+			
+			if(!lines.isEmpty())
+				toWrite += lines.get(lines.size() - 1) + "\n";*/
+			
+			String toWrite = "";
+			
+			toWrite = "[" + name + "{}]";
+			
+			editor.sendMessage("Writing: " + toWrite);
+			
+			Files.write(file.toPath(), toWrite.getBytes(), StandardOpenOption.APPEND);
+			
+			//fileWriter.write(toWrite);
+			//fileWriter.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return line;
+	}
+	
+	public void updateFile(Location loc, String cmd) {
+		editor.sendMessage("Update file");
+		try {
+			Scanner fileRead = new Scanner(file);
+			String line = fileRead.nextLine();
+			
+			//String toWrite = "[" + name + "{" + "(" + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + "," + cmd + "}]";
+			
+			ArrayList<String> lines = new ArrayList<String>(Files.readAllLines(file.toPath()));
+			editor.sendMessage("lines: " + Arrays.toString(lines.toArray()));
+			editor.sendMessage("currentLine: " + currentLine);
+			
+			for(int i = 0; i < currentLine; i++)
+				line = fileRead.nextLine();
+			
+			String toWrite = line;
+			
+			editor.sendMessage("fileRead: " + line);
+			
+			for(int i = 0; i < lines.size(); i++)
+				if(lines.get(i).equalsIgnoreCase(line))
+				{
+					// Checking if this is empty or not
+					if(line.toCharArray()[line.indexOf("{") + 1] == '}')
+						toWrite = toWrite.substring(0, toWrite.indexOf("{") + 1);
+					else
+						toWrite = toWrite.substring(0, toWrite.lastIndexOf(";") + 1);
+					
+					toWrite += "(" + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + "," + cmd + ");}]";
+					lines.set(i, toWrite);
+				}
+			//fileWriter.
+			
+			//String toWrite = "[" + name + "{" + "(" + loc.getBlockX() + "," + loc.getBlockY() + "," + loc.getBlockZ() + "," + cmd + "}]";
+			
+			fileRead.close();
+			Files.write(file.toPath(), lines, StandardCharsets.UTF_8);
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int checkIfExists() {
+		if(file.length() == 0)
+		{
+			editor.sendMessage("File empty as hell");
+			appendFile(0);
+			return -1;
+		}
+		
+		int found = 0;
+		editor.sendMessage("Checking if group exists");
+		
+		try {
+			Scanner fileRead = new Scanner(file);
+			String line = fileRead.nextLine();
+			boolean loop = true;
+			
+			editor.sendMessage("First line: " + line);
+			
+			do {
+				editor.sendMessage("Has next line");
+				String lineName = line.substring(line.indexOf("[") + 1, line.indexOf("{"));
+				editor.sendMessage(lineName);
+				editor.sendMessage(line);
+				
+				if(name.equalsIgnoreCase(lineName))
+				{
+					editor.sendMessage("Line found " + found);
+					return found;
+				}
+				
+				found++;
+				loop = fileRead.hasNextLine();
+				line = loop ? fileRead.nextLine() : null;
+			} while(loop);
+				
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		return appendFile(found);
 	}
 
 	public Player getEditor() {
